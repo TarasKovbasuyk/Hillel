79417e8780faefef957aa976504aa9f743f4de03
Ковбасюк Тарас <54114460+TarasKovbasuyk@users.noreply.github.com>
2019-08-18 16:09:50 +0300

    Add files via upload

---
+++ lesson3-4.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+<form name="a" method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+    <input type="text" name="a">
+    <input type="submit">
+</form>
+<?php
+$str_name = $_GET ['a'];
+echo '<br><hr>';
+$fio = explode(' ', $str_name);
+echo $fio[0] . ' ' . substr($fio[1],0,2) . '.' . substr($fio[2],0,2) . '.' ;
+
