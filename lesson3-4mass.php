3a1f52f2f1ee86a0ad50711ee28945bde483e23e
Ковбасюк Тарас <54114460+TarasKovbasuyk@users.noreply.github.com>
2019-08-19 14:44:24 +0300

    Add files via upload

---
+++ lesson3-4mass.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+</head>
+<body>
+<form name="a" method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+    <input type="text" name="a">
+    <input type="submit">
+</form>
+<?php
+$name = $_GET['a'];
+$mass_name = explode(" ",$name);
+foreach ($mass_name as $n) {
+    $countMassive = count($mass_name);
+    if ($countMassive > 1) {
+        for ($i = 1; $i < $countMassive; $i++) {
+            $mass_name[$i] = mb_substr($mass_name[$i], 0, 1) . '.';
+        }
+        $fio = implode(' ', $mass_name);
+    }
+}
+echo ($fio);
+
+
+
