c2bdfb6be8c1807dfb9d18125add8364e3112347
Ковбасюк Тарас <54114460+TarasKovbasuyk@users.noreply.github.com>
2019-08-19 16:06:10 +0300

    Add files via upload

---
+++ ass_massive.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+</head>
+<body>
+<form name="a" method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+    <p>Введите ru/eng</p>
+    <input type="text" name="a">
+    <input type="submit">
+    <p>Введите день недели (1-7)</p>
+    <input type="text" name="b">
+    <input type="submit">
+</form>
+<?php
+$lang = $_GET["a"];
+$day =  $_GET["b"];
+$mas["ru"] =array("1"=>"Понедельник","2"=>"Вторник","3"=>"Среда","4"=>"Четверг",
+    "5"=>"Пятница","6"=>"Суббота","7"=>"Воскресенье");
+$mas["eng"]= array("1"=>"Monday","2"=>"Tuesday","3"=>"Wednesday","4"=>"Thursday",
+    "5"=>"Friday","6"=>"Saturday","7"=>"Sunday");
+echo $mas[$lang]["$day"];
+
+
