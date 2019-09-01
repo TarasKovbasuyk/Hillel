fe87890f3a2f8cdce1018dd9bd45a98240ce931e
Ковбасюк Тарас <54114460+TarasKovbasuyk@users.noreply.github.com>
2019-09-01 15:05:38 +0300

    Update homework10-2.php

--- lesson6hw-10/homework10-2.php
+++ lesson6hw-10/homework10-2.php
@@ -1,1 +1,30 @@
+<body>
+<form method="POST">
+    <p> Введите число </p>
+    <input type="text" name="num">
+    <input type="submit">
+</form>
+</body>
+</html>
+<?php
+$num = $_POST['num'];
+function reverse(int $num)
+{
+    static $reverseNumI = 0;
+    $i = 0;
+    $reverseNumI++;
+    if ($num > 0) {
+        while ($i < $reverseNumI) {
+            if ($num - 1 > 0) {
+                echo $reverseNumI . ", ";
+            } elseif ($num - 1 == 0) {
+                echo $reverseNumI;
+            }
+            $i++;
+            $num--;
+        };
+        reverse($num);
+    }
+}
 
+reverse($num);
