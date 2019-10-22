ac27b4c16671949feea7f0e069ed21cad342df2d
Ковбасюк Тарас <54114460+TarasKovbasuyk@users.noreply.github.com>
2019-10-22 11:04:04 +0300

    Update CsvRead.php

--- homework17/CsvRead.php
+++ homework17/CsvRead.php
@@ -1,23 +1,28 @@
 <?php
 require_once('Read.php');
 
-class CsvRead implements Read
+class CsvRead implements \Read
 {
-    public function read($file)
+    
+    public function read(string $file)
+    {
+        if (($handle = fopen($file, "r")) !== FALSE) {
+            return $this->showTable($handle);
+            fclose($handle);
+        } else {
+            throw new Exception('File not found');
+        }
+    }
+    public function showTable($handle):void
     {
         echo '<html> <head><meta charset="utf-8"></head> <body> <table border="1"> <caption>Список сотрудников</caption>';
-        if (($handle = fopen($file, "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000)) !== FALSE) {
-                foreach ($data as $key => $value) {
-                    $pieces = explode(";", $value);
-                    echo '<tr> <td>' . $pieces[0] . '</td>';
-                    echo '<td> ' . $pieces[1] . '</td> </tr>';
-                }
+        while (($data = fgetcsv($handle, 1000)) !== FALSE) {
+            foreach ($data as $key => $value) {
+                $pieces = explode(";", $value);
+                echo '<tr> <td>' . $pieces[0] . '</td>';
+                echo '<td> ' . $pieces[1] . '</td> </tr>';
             }
-            echo '</table> </body> </html>';
-            fclose($handle);
         }
-
+        echo '</table> </body> </html>';
     }
-
 }
